a0fedd10c37d0d7848da7629176f1e6aacb4adfa
Miller <miltonycchow@gmail.com>
2024-05-25 10:25:40 +0800

    feat: checkpoint for success npm run dev

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,12 @@
+const path = require('path')
 module.exports = {
     i18n: {
         locales: ['en', 'jp'],
         defaultLocale: 'en',
-    }
+        localeDetection: false
+    },
+    localePath: path.resolve('./public/locales'),
+    output: 'export',
+    basePath: '/the-website'
 
 }
